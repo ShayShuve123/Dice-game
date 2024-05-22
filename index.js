5995b9e4f629156c99ebf67b7959346292e51b9a
ShayShuve123 <129605144+ShayShuve123@users.noreply.github.com>
2024-05-22 23:47:23 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-var randomNumber1 = Math.floor((Math.random() *6) + 1);
-var pathImg1= "./images/dice"+randomNumber1+".png"
-document.querySelector("img.img1").setAttribute("src",pathImg1)
+var randomNumber1;
+var randomNumber2;
 
-var randomNumber2 = Math.floor((Math.random() *6) + 1);
-var pathImg2= "./images/dice"+randomNumber2+".png"
-document.querySelector("img.img2").setAttribute("src",pathImg2)
+function playWinnerSound() {
+    var winnerAudio = document.getElementById("winnerSound");
+    winnerAudio.play();
+  }
 
-//who is the winner?
+function rollDice() {
+    randomNumber1 = Math.floor(Math.random() * 6) + 1;
+    var pathImg1 = "./images/dice" + randomNumber1 + ".png";
+    document.querySelector("img.img1").setAttribute("src", pathImg1);
+  
+    randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    var pathImg2 = "./images/dice" + randomNumber2 + ".png";
+    document.querySelector("img.img2").setAttribute("src", pathImg2);
+
+    //who is the winner?
 if(randomNumber1==randomNumber2){
     document.querySelector("h1").textContent="🥳Draw🥳"
 }
 else if(randomNumber1>randomNumber2){
     document.querySelector("h1").textContent="player 1 wins🏆"
+    playWinnerSound(); 
 
 }
 else{
     document.querySelector("h1").textContent="player 2 wins🏆"
+    playWinnerSound(); 
 }
+}
+
+
+
+document.querySelector("button.btn").addEventListener("click", rollDice);
